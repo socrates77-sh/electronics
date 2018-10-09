521841f54d4131e6390ed54ac008eceb237effdb
socrates77 <zhwenrong@sina.com>
2018-10-09 16:14:57 +0800

    done p4_2.py @office
    Signed-off-by: socrates77 <zhwenrong@sina.com>

---
+++ gray/p4_2.py
@@ -0,0 +1,30 @@
+from sympy import symbols, evalf, solve, diff, log
+from sympy.plotting import plot
+
+IS = 5e-15
+VA = 130
+VBE = 0.7
+bf = 200
+
+VCC = 15
+R = 10e3
+
+Vout, Iout = symbols('Vout Iout')
+VCE1 = VBE
+VCE2 = Vout
+IC1 = (VCC - VBE) / R
+IS1 = 2 * IS
+IS2 = 3 * IS
+
+
+Iout = (IS2 / IS1) * IC1 * (1 + (VCE2 - VCE1) / VA) / \
+    (1 + (1 + IS2 / IS1) / bf)
+
+Iout_1 = Iout.evalf(subs={Vout: 1})
+Iout_2 = Iout.evalf(subs={Vout: 5})
+Iout_3 = Iout.evalf(subs={Vout: 30})
+
+print('Iout=%f (mA)' % (Iout_1 * 1e3))
+print('Iout=%f (mA)' % (Iout_2 * 1e3))
+print('Iout=%f (mA)' % (Iout_3 * 1e3))
+# plot(Iout, xlim=(-100, 50), ylim=(-10e-3, 10e-2))
