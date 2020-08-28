4173f14eb2f9b039df5aa1895de589a7553baca7
socrates77 <zhwenrong@sina.com>
2020-08-28 19:16:18 +0800

    done p4_15.py

---
+++ gray/p4_14.py
@@ -0,0 +1,44 @@
+from sympy import symbols, solve, diff, log
+from sympy.plotting import plot
+from resp import *
+from math import *
+
+un = 550e-4
+eox = 3.9*8.86e-12
+esi = 11.6*8.86e-12
+tox = 150e-10
+dXd_n = 0.08e-6
+dXd_p = 0.04e-6
+Ld_n = 0.12e-6
+Ld_p = 0.18e-6
+NA = 5e21
+ni = 1.45e16
+q = 1.6e-19
+VT = 26e-3
+
+Ldrw = 1e-6
+W2 = 50e-6
+W4 = 100e-6
+Itail = 100e-6
+
+Leff2 = Ldrw - 2*Ld_n
+Leff4 = Ldrw - 2*Ld_p
+
+Cox = eox/tox
+lamb_n = dXd_n/Leff2
+lamb_p = dXd_p/Leff4
+
+ID = 0.5*Itail
+
+ro2 = 1/(lamb_n*ID)
+ro4 = 1/(lamb_p*ID)
+
+Rout = resp(ro2, ro4)
+
+gm = sqrt(2*ID*un*Cox*W2/Leff2)
+Av = gm*Rout
+
+print('Rout=%fohm' % (Rout*1e-3))
+print('Av=%f' % Av)
+
+# 79
