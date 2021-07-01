64a35d994e71a4e9bbdf95199bd88d29fc75ed3c
socrates77 <zhwenrong@sina.com>
2021-07-01 09:40:54 +0800

    done p7_3.py @office
    Signed-off-by: socrates77 <zhwenrong@sina.com>

--- gray/p7_3.py
+++ gray/p7_3.py
@@ -4,6 +4,7 @@ from resp import *
 from math import *
 
 import matplotlib.pyplot as plt
+import numpy as np
 from sympy import *
 from control import *
 
@@ -18,15 +19,7 @@ Cu = 0.3e-12
 
 gm = IC/VT
 rpi = beta/gm
-Cpi = gm/(2*pi*fT)-Cu
-# CM = (1+gm*RL)*Cu
-# f_3db = 1/(2*pi*resp(Rs+rb, rpi)*(CM+Cpi))
-
-# R=resp(Rs+rb, rpi)
-# p2=-(1/(RL*Cu)+1/(R*Cpi)+1/(RL*Cpi)+gm/Cpi)/(2*pi)
-
-# print('f-3db=%f (MHz)' % (f_3db*1e-6))
-# print('p2=%f (MHz)' % (p2*1e-6))
+Cpi = gm/(2*3.1416*fT)-Cu
 R = resp(Rs+rb, rpi)
 Cx = gm*R*Cu
 Rx = (1+Cpi/Cu)/gm
@@ -35,12 +28,13 @@ Rx = (1+Cpi/Cu)/gm
 
 s, B1 = symbols('s B1')
 # B1 = resp(1/(Cu*s), 1/gm*(1/(R*Cu*s)+Cpi/Cu+1))
-Z1=1/(Cu*s)
-Z2=1/(gm*R*Cu*s)+(1+Cpi/Cu)/gm
-B1=Z1*Z2/(Z1+Z2)
-
-# B1 = 25 * s / (2 * s**2 + 3 * s + 3)
+Z1 = 1/(Cu*s)
+Z2 = 1/(gm*R*Cu*s)+(1+Cpi/Cu)/gm
+B10 = Z1*Z2/(Z1+Z2)
+# B1=(1+s*Rx*Cx)/(s*(Cx+Cu)+s*s*Rx*Cx*Cu)
+B1 = simplify(B10)
 # print(B1)
+# B1 = 25 * s / (2 * s**2 + 3 * s + 3)
 D0 = denom(B1)
 D1 = Poly(D0).all_coeffs()
 D2 = [float(k) for k in D1]
@@ -48,7 +42,19 @@ N0 = numer(B1)
 N1 = Poly(N0).all_coeffs()
 N2 = [float(k) for k in N1]
 B1b = tf(N2, D2)
-bode(B1b, dB=True)
+
+# print(D0)
+# print(N0)
+# print(D2)
+# print(N2)
+print(B1b)
+f = np.logspace(0, 13, num=100)
+mag, phase, omega = bode(B1b, dB=False, Hz=True, omega=f)
+print(np.logspace(0, 10, num=100))
+# bode(B1b, omega=(1e5, 1e10), Hz=True,  dB=False)f
+# print(mag)
+# print(omega)
+
 plt.show()
 
 
